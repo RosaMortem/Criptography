532e12c0ab48d401bd3f41b30a1654bcbc5bdba0
Agriel <rodionkrikun@yandex.ru>
2025-03-15 08:01:53 +0300

    Rewrite the functions of the programm and add txt-s

--- Labs/lab1.py
+++ Labs/lab1.py
@@ -7,41 +7,37 @@ def rotate_right(value, shift, bit_size=16):
     return ((value >> shift) | (value << (bit_size - shift))) & (2**bit_size - 1)
 def F(x0, x1, x2):
     not_x0 = (~x0)
-    x0_x1 = not_x0 ^ (x1.rotate_left(x1, 5)) %  (2**16)
-    x0_x1_x2 = x0_x1 & x2.rotate_right(x2, 9)
+    x0_x1 = not_x0 ^ (rotate_left(x1, 5)) %  (2**16)
+    x0_x1_x2 = x0_x1 & rotate_right(x2, 9)
     return x0_x1_x2
 def generate_round_key(K_64, i):
-    first_shift = i * 5
-    second_shift  = i * 2
-    shifted_key = K_64 >> first_shift
-    lower_32 = shifted_key & (2**32 - 1)
-    upper_32 = (shifted_key >> 32) & (2**32 - 1)
-    xor_result = lower_32 ^ upper_32
-    return xor_result & (2**16 - 1)
-def feistel_round(m0, m1, m2, m3, key, round_num):
+    shift_1 = i * 5
+    shift_2  = i * 2
+    shifted_key1 = K_64 << shift_1
+    shifted_key2 = K_64 >> shift_2
+    res = (shifted_key1 ^ shifted_key2) & (2**16 - 1)
+    return res
+def feistel_round(x0, x1, x2, x3, key, round_num):
     k_i = generate_round_key(key, round_num)
-    f1_result = F1(m2, m3)
-    f2_result = F2(m0, m1)
-    new_m0 = m0 ^ k_i ^ f1_result
-    new_m1 = m1 ^ k_i ^ f2_result
-    new_m2 = m2 ^ (k_i >> 3)
-    new_m3 = m3 ^ (k_i >> 3)
-    return new_m0, new_m1, new_m2, new_m3
+    F_result = F(x0, x1, x2)
+    new_x0 = x3 ^ F_result
+    new_x1 = x0
+    new_x2 = x1 ^ k_i
+    new_x3 = x2 ^ x3
+    return new_x0, new_x1, new_x2, new_x3
 def feistel_encrypt_block(block, key, rounds):
-    m0, m1, m2, m3 = block
+    x0, x1, x2, x3 = block
     for i in range(rounds):
-        m0, m1, m2, m3 = feistel_round(m0, m1, m2, m3, key, i)
-    return m0, m1, m2, m3
+        x0, x1, x2, x3 = feistel_round(x0, x1, x2, x3, key, i)
+    return x0, x1, x2, x3
 def encrypt_decrypt_OFB(data_blocks, key, iv, rounds=4):
     result_blocks = []
     feedback = iv
-
     for block in data_blocks:
         encrypted_feedback = feistel_encrypt_block(feedback, key, rounds)
         result_block = tuple(a ^ b for a, b in zip(block, encrypted_feedback))
         result_blocks.append(result_block)
         feedback = encrypted_feedback
-
     return result_blocks
 
 def read_text_blocks(filename):
@@ -52,11 +48,11 @@ def read_text_blocks(filename):
         block = data[i:i+8]
         if len(block) < 8:
             block += bytes(8 - len(block))
-        m0 = int.from_bytes(block[:2], 'big')
-        m1 = int.from_bytes(block[2:4], 'big')
-        m2 = int.from_bytes(block[4:6], 'big')
-        m3 = int.from_bytes(block[6:], 'big')
-        blocks.append((m0, m1, m2, m3))
+        x0 = int.from_bytes(block[:2], 'big')
+        x1 = int.from_bytes(block[2:4], 'big')
+        x2 = int.from_bytes(block[4:6], 'big')
+        x3 = int.from_bytes(block[6:], 'big')
+        blocks.append((x0, x1, x2, x3))
     return blocks
 
 def write_text_blocks(blocks, filename):
